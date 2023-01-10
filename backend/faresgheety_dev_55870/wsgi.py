9832618884b4ae497f459de6910c610817246df5
Crowdbotics <engineering@crowdbotics.com>
2023-01-10 03:48:49 +0000

    Initial commit.

---
+++ backend/faresgheety_dev_55870/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for faresgheety_dev_55870 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'faresgheety_dev_55870.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "faresgheety-dev-55870",
+  url: "https://faresgheety-dev-55870.botics.co",
+  api: "https://faresgheety-dev-55870.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
